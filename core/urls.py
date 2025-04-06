1c32426ebc52b5726c298b7ed1f2e5ad0be38f22
Marcos <onetestt6@gmail.com>
2025-04-06 11:30:32 -0300

    start pagina teste user sem login

--- core/urls.py
+++ core/urls.py
@@ -4,13 +4,13 @@ from django.views.generic.base import RedirectView
 from . import views
 
 urlpatterns = [
-    path('', views.landing_page, name='landing',),
+    path('', views.landing_page, name='landing'),
     path('admin/', admin.site.urls),
     path('usuarios/', include("apps.usuarios.urls")),
     path("exercicios/", include("apps.exercicios.urls",)),
     path("desafios/", include("apps.desafios.urls",)),
     path("ranking/", include("apps.ranking.urls")),
     path("login/", include("apps.usuarios.urls")),
-
+    path('teste/', views.acesso_sem_login, name='acesso_sem_login'),
     # path('', include('apps.usuarios.urls')),
 ]
--- core/views.py
+++ core/views.py
@@ -2,3 +2,8 @@ from django.shortcuts import render, redirect
 
 def landing_page(request):
     return render(request, 'landing.html')
+
+def acesso_sem_login(request):
+    
+    return render(request, 'acesso_sem_login.html')
+    
