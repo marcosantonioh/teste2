771173c14bb5cfcfc85e8ae4313ec2b457415648
Marcos Antônio <onetestt6@gmail.com>
2025-07-11 11:43:23 -0300

    Add STATIC_ROOT configuration and update STATICFILES_STORAGE for WhiteNoise

--- core/settings.py
+++ core/settings.py
@@ -145,9 +145,14 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = '/static/'
+
+# O caminho absoluto para a pasta onde o `collectstatic` vai juntar os arquivos
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),  # Diretório global de arquivos estáticos
 ]
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
